ae0fb102bdb9ce4d32e6a14f9a2a0b12abb7d827
jfet97 <andrysimo1997gmail.com>
2018-06-11 20:03:16 +0200

    Add web page

---
+++ docs/js/custom.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+    /*
+    var w = window.innerWidth;
+    var logo_container = document.getElementsByClassName("logo-container");
+    
+    if(w >= 1000) {
+        logo_container[0].classList.add("sb-each-item-40-nogs");
+    }
+
+    if((w < 1000)&&(w >= 600)) {
+        logo_container[0].classList.add("sb-each-item-30-nogs");
+    }
+
+    if(w < 600) {
+        logo_container[0].classList.add("sb-each-item-20-nogs");
+    } */
+  
+}
+
+window.onresize = function() {
+
+    /*
+    var w = window.innerWidth;
+    var logo_container = document.getElementsByClassName("logo-container");
+
+    
+    if(w >= 1000) {
+        logo_container[0].classList.toggle("sb-each-item-30-nogs", false);
+        logo_container[0].classList.toggle("sb-each-item-20-nogs", false);
+        logo_container[0].classList.add("sb-each-item-40-nogs");
+    }
+
+    if((w < 1000)&&(w >= 600)) {
+        logo_container[0].classList.toggle("sb-each-item-40-nogs", false);
+        logo_container[0].classList.toggle("sb-each-item-20-nogs", false);
+        logo_container[0].classList.add("sb-each-item-30-nogs");
+    }
+
+    if(w < 600) {
+        logo_container[0].classList.toggle("sb-each-item-40-nogs", false);
+        logo_container[0].classList.toggle("sb-each-item-30-nogs", false);
+        logo_container[0].classList.add("sb-each-item-20-nogs");
+    }
+    */
+
+}
